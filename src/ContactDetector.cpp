21b4d127c3d8b3e475d0279f3e5807f02b179b73
Fritz NAGAE <fritz1414213562@gmail.com>
2025-09-03 06:18:12 +0900

    fix: inherit native distances of previous pairs to next pairs

--- src/ContactDetector.cpp
+++ src/ContactDetector.cpp
@@ -6,28 +6,38 @@
 ContactDetector::result_type ContactDetector::run(
 	const Coordinate& coordinate,
 	const float cutoff, const std::size_t ignore_num,
-	const std::size_t max_contact, const ContactDetector::indices_type& previous_pairs) const
+	const std::size_t max_contact, const ContactDetector::ff_type& previous_ff) const
 {
-	const ContactDetector::indices_type& contact_pair_list
-		= detect_contact_pairs(coordinate, cutoff, ignore_num, previous_pairs);
-	if (contact_pair_list.empty() or (contact_pair_list.size() <= max_contact))
-		return collect_indices_and_parameters(coordinate, contact_pair_list);
+	const ContactDetector::ff_type& contact_pair_data
+		= detect_contact_pairs(coordinate, cutoff, ignore_num, previous_ff);
+	if (contact_pair_data.first.size() != contact_pair_data.second.size())
+		throw std::runtime_error(
+			"[error] The parameter number of indices must be the same as that of native-distance");
+	if (contact_pair_data.first.empty() or (contact_pair_data.first.size() <= max_contact))
+		return std::make_tuple(
+			contact_pair_data.first,
+			std::vector<float>(contact_pair_data.first.size(), theta0_),
+			std::vector<float>(contact_pair_data.first.size(), phi0_),
+			contact_pair_data.second);
 
 	std::mt19937_64 engine(seed_);
 	const std::vector<std::size_t>& shuffled_indices
 		= Utility::fisher_yates_random_choice(
-			max_contact, 0, contact_pair_list.size() - 1, engine);
-	ContactDetector::indices_type retval;
+			max_contact, 0, contact_pair_data.first.size() - 1, engine);
+	ContactDetector::result_type retval;
 	for (const auto& index : shuffled_indices)
 	{
-		retval.push_back(contact_pair_list[index]);
+		std::get<0>(retval).push_back(contact_pair_data.first[index]);
+		std::get<1>(retval).push_back(theta0_);
+		std::get<2>(retval).push_back(phi0_);
+		std::get<3>(retval).push_back(contact_pair_data.second[index]);
 	}
-	return collect_indices_and_parameters(coordinate, retval);
+	return retval;
 }
 
-ContactDetector::indices_type ContactDetector::detect_contact_pairs(
+ContactDetector::ff_type ContactDetector::detect_contact_pairs(
 	const Coordinate& coordinate, const float cutoff,
-	const std::size_t ignore_num, const ContactDetector::indices_type& previous_pairs) const
+	const std::size_t ignore_num, const ContactDetector::ff_type& previous_ff) const
 {
 	const std::size_t& natom = coordinate.atom_num();
 	if (ignore_num < 2)
@@ -35,15 +45,20 @@ ContactDetector::indices_type ContactDetector::detect_contact_pairs(
 	else if (natom < ignore_num + 1)
 		throw std::runtime_error("[error] 'ignore_num' must be smaller than n_atoms.");
 
-	ContactDetector::indices_type retval;
+	ContactDetector::indices_type contact_indices;
+	ContactDetector::param_type   native_distances;
 	std::unordered_set<std::size_t> black_list;
-	for (const auto& pair : previous_pairs)
+	for (std::size_t ipair = 0; ipair < previous_ff.first.size(); ++ipair)
 	{
+		const auto& pair = previous_ff.first[ipair];
+		const auto& native_distance = previous_ff.second[ipair];
 		const std::size_t& idx = pair[0];
 		const std::size_t& jdx = pair[2];
-		if (coordinate.distance(idx, jdx) < cutoff)
+		const float& dist = coordinate.distance(idx, jdx);
+		if (dist < cutoff)
 		{
-			retval.push_back({idx, idx + 1, jdx, jdx + 1});
+			contact_indices.push_back({idx, idx + 1, jdx, jdx + 1});
+			native_distances.push_back(native_distance);
 			black_list.insert(idx);
 			black_list.insert(jdx);
 		}
@@ -54,34 +69,36 @@ ContactDetector::indices_type ContactDetector::detect_contact_pairs(
 		for (std::size_t jdx = idx + ignore_num; jdx < natom - 1; ++jdx)
 		{
 			if (black_list.find(jdx) != black_list.end()) continue;
-			if (coordinate.distance(idx, jdx) < cutoff) 
+			const float& dist = coordinate.distance(idx, jdx);
+			if (dist < cutoff) 
 			{
-				retval.push_back({idx, idx + 1, jdx, jdx + 1});
+				contact_indices.push_back({idx, idx + 1, jdx, jdx + 1});
+				native_distances.push_back(dist);
 				black_list.insert(idx);
 				black_list.insert(jdx);
 			}
 		}
 	}
-	return retval;
+	return std::make_pair(contact_indices, native_distances);
 }
 
-ContactDetector::result_type ContactDetector::collect_indices_and_parameters(
-	const Coordinate& coordinate, const ContactDetector::indices_type& contact_pairs) const
-{
-	ContactDetector::param_type theta0s;
-	ContactDetector::param_type phi0s;
-	ContactDetector::param_type r0s;
-	for (const auto& pair : contact_pairs)
-	{
-		const float r = coordinate.distance(pair[0], pair[2]);
-		const float theta = coordinate.dihedral(pair[1], pair[0], pair[2], pair[3]);
-		const float phi   = coordinate.angle(pair[1], pair[0], pair[2])
-						  - coordinate.angle(pair[0], pair[2], pair[3]);
-		r0s.push_back(r);
-		if (theta > -0.5 * pi_ && theta < 0.5 * pi_) theta0s.push_back(0.0f);
-		else theta0s.push_back(pi_);
-		if (phi   > -0.5 * pi_ && phi   < 0.5 * pi_) phi0s.push_back(0.0f);
-		else phi0s.push_back(pi_);
-	}
-	return std::make_tuple(contact_pairs, theta0s, phi0s, r0s);
-}
+//ContactDetector::result_type ContactDetector::collect_indices_and_parameters(
+//	const Coordinate& coordinate, const ContactDetector::indices_type& contact_pairs) const
+//{
+//	ContactDetector::param_type theta0s;
+//	ContactDetector::param_type phi0s;
+//	ContactDetector::param_type r0s;
+//	for (const auto& pair : contact_pairs)
+//	{
+//		const float r = coordinate.distance(pair[0], pair[2]);
+//		const float theta = coordinate.dihedral(pair[1], pair[0], pair[2], pair[3]);
+//		const float phi   = coordinate.angle(pair[1], pair[0], pair[2])
+//						  - coordinate.angle(pair[0], pair[2], pair[3]);
+//		r0s.push_back(r);
+//		if (theta > -0.5 * pi_ && theta < 0.5 * pi_) theta0s.push_back(0.0f);
+//		else theta0s.push_back(pi_);
+//		if (phi   > -0.5 * pi_ && phi   < 0.5 * pi_) phi0s.push_back(0.0f);
+//		else phi0s.push_back(pi_);
+//	}
+//	return std::make_tuple(contact_pairs, theta0s, phi0s, r0s);
+//}
--- src/ContactDetector.hpp
+++ src/ContactDetector.hpp
@@ -6,11 +6,13 @@
 #include <array>
 #include <tuple>
 #include <cmath>
+#include <utility>
 
 class ContactDetector
 {
 	using indices_type = std::vector<std::array<std::size_t, 4>>;
 	using param_type   = std::vector<float>;
+	using ff_type      = std::pair<indices_type, param_type>;
 	using result_type  = std::tuple<indices_type, param_type, param_type, param_type>;
 
 	public:
@@ -18,18 +20,20 @@ class ContactDetector
 		~ContactDetector() = default;
 		result_type run(const Coordinate& coordinate,
 			const float cutoff, const std::size_t ignore_num,
-			const std::size_t max_contact, const indices_type& previous_pairs) const;
+			const std::size_t max_contact, const ff_type& previous_ff) const;
 
 	private:
-		indices_type detect_contact_pairs(const Coordinate& coordinate,
+		ff_type detect_contact_pairs(const Coordinate& coordinate,
 			const float cutoff, const std::size_t ignore_num,
-			const indices_type& previous_pairs) const;
-		result_type  collect_indices_and_parameters(
-			const Coordinate& coordinate, const indices_type& contact_pairs) const;
+			const ff_type& previous_ff) const;
+		//result_type  collect_indices_and_parameters(
+		//	const Coordinate& coordinate, const ff_type& contact_pairs) const;
 
 	private:
 		int seed_;
-		float pi_ = std::acos(-1.0);
+		//float pi_ = std::acos(-1.0);
+		float theta0_ = 0.0;
+		float phi0_ = 0.0;
 };
 
 
