3888598a97a74dfe9963ed90d0e0307d66f413b5
Fritz NAGAE <fritz1414213562@gmail.com>
2025-09-03 06:18:29 +0900

    fix: read native distance

--- src/io/forcefield_reader.cpp
+++ src/io/forcefield_reader.cpp
@@ -1,11 +1,12 @@
 #include "forcefield_reader.hpp"
 #include "src/util/utility.hpp"
 
-ForceFieldReader::indices_type ForceFieldReader::read(const std::string& filename) const
+ForceFieldReader::result_type ForceFieldReader::read(const std::string& filename) const
 {
 	const toml::value& data = toml::parse(filename);
 	const auto& ff = toml::find(data, "forcefields").at(0);
-	ForceFieldReader::indices_type retval;
+	ForceFieldReader::indices_type indices_set;
+	std::vector<float> v0s;
 	if (ff.contains("local"))
 	{
 		const auto& locals = toml::find(ff, "local").as_array();
@@ -21,10 +22,12 @@ ForceFieldReader::indices_type ForceFieldReader::read(const std::string& filenam
 				{
 					const auto& indices = Utility::find_parameter<std::array<std::size_t, 4>>(
 						param, env, "indices");
-					retval.push_back(indices);
+					indices_set.push_back(indices);
+					const auto& v0 = Utility::find_parameter<float>(param, env, "v0");
+					v0s.push_back(v0);
 				}
 			}
 		}
 	}
-	return retval;
+	return std::make_pair(indices_set, v0s);
 }
--- src/io/forcefield_reader.hpp
+++ src/io/forcefield_reader.hpp
@@ -7,15 +7,17 @@
 #include <string>
 #include <vector>
 #include <array>
+#include <utility>
 
 class ForceFieldReader
 {
 	using indices_type = std::vector<std::array<std::size_t, 4>>;
+	using result_type  = std::pair<indices_type, std::vector<float>>;
 	public:
 		ForceFieldReader() = default;
 		~ForceFieldReader() = default;
 
-		indices_type read(const std::string& filename) const;
+		result_type read(const std::string& filename) const;
 };
 
 #endif // MONTE_CARLO_FF_REGULATOR_FORCE_FIELD_READER_HPP
